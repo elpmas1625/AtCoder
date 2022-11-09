20959b028e389d1accac9271a2eb8576ae725825
elpmas1625 <elpmas.1625@gmail.com>
2022-11-09 15:16:15 +0900

    add ex13.cpp

---
+++ cpp/apg4b/ex14.cpp
@@ -0,0 +1,7 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << max(1, 2) << endl;
+}
---
+++ cpp/newcpp.sh
@@ -0,0 +1,9 @@
+touch $1/$2.cpp
+cat > $1/$2.cpp << EOF
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+}
