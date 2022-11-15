45dfbb612ba3dfc2b4aaccbe5ed7ffcbab231e35
elpmas1625 <elpmas.1625@gmail.com>
2022-11-15 23:29:19 +0900

    apg4b ex20

--- cpp/apg4b/ex20.cpp
+++ cpp/apg4b/ex20.cpp
@@ -1,29 +1,37 @@
-#define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    vector<vector<int>> vec(n);
-    vector<vector<int>> tmp = {
-        {},
-        {},
-        {},
-    };
+// x番の組織が親組織に提出する枚数を返す
+// childrenは組織の関係を表す2次元配列(参照渡し)
+int count_report_num(vector<vector<int>> &children, int x) {
+  // (ここに追記して再帰関数を実装する)
 
-    int p;
-    for (int i=1; i<n; i++){
-        cin >> p;
-        vec[p].push_back(i);
+    int ans = 0;
+    if 
+    return ans;
+}
+
+// これ以降の行は変更しなくてよい
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> p(N);  // 各組織の親組織を示す配列
+    p.at(0) = -1;  // 0番組織の親組織は存在しないので-1を入れておく
+    for (int i = 1; i < N; i++) {
+        cin >> p.at(i);
     }
 
+  // 組織の関係から2次元配列を作る
+    vector<vector<int>> children(N);  // ある組織の子組織の番号一覧
+    for (int i = 1; i < N; i++) {
+        int parent = p.at(i);  // i番の親組織の番号
+        children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
+    }
 
-    for (int i=0; i<vec.size(); i++){
-        for (int j=0; j<vec[i].size(); j++){
-            cout << vec[i][j];
-            if (j!= n) cout << ' ';
-        }
-        cout << endl;
+  // 各組織について、答えを出力
+    for (int i = 0; i < N; i++) {
+        cout << count_report_num(children, i) << endl;
     }
 }
